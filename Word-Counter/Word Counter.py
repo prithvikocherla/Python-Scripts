67face66540102d9bbda991afbfb4cacb0704f76
prithvikocherla <47038576+prithvikocherla@users.noreply.github.com>
2019-04-24 09:06:16 -0400

    Update Word Counter.py

--- Word-Counter/Word Counter.py
+++ Word-Counter/Word Counter.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import pandas as pd
 from collections import OrderedDict
 
@@ -16,34 +13,16 @@ for i in df:
     for x in words:
         text_list.append(x)  
 
-
-# In[2]:
-
-
 dict_words = {}
 for i in text_list:
     dict_words.update({i:text_list.count(i)})
 dict_words_sorted = OrderedDict(sorted(dict_words.items(), key=lambda x: x[1], reverse=True))
 
-
-# In[7]:
-
-
 def freqcount(x):    
     for x, y in list(dict_words_sorted.items())[0:x]:
         print("{}: {}".format(x,y))
 
-
-# In[8]:
-
-
 var = input('Please specify the number of top words to be displayed: ')
 print("")
 freqcount(int(var))
-
-
-# In[ ]:
-
-
-
 
